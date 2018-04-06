dc2417b7e263f5e803407d08c37447cf2fea2fff
konstantinov90 <konstantinov90@list.ru>
2018-04-06 21:05:30 +0300

    intermediate commit

--- src/ts/extend.ts
+++ src/ts/extend.ts
@@ -1,5 +1,9 @@
-const hasOwnProperty: (prop: string) => boolean = Object.prototype.hasOwnProperty;
+const hasOwnProperty: (k: string) => boolean = Object.prototype.hasOwnProperty;
 const toString: () => string = Object.prototype.toString;
+
+interface ISomeObject {
+    [key: string]: any;
+}
 
 /**
  * Проверяет, что переданный объект является "плоским" (т.е. созданным с помощью "{}"
@@ -8,7 +12,7 @@ const toString: () => string = Object.prototype.toString;
  * @param {Object} obj
  * @returns {Boolean}
  */
-function isPlainObject(obj: object): boolean {
+function isPlainObject(obj: any): boolean {
     if (toString.call(obj) !== '[object Object]') {
         return false;
     }
@@ -27,32 +31,45 @@ function isPlainObject(obj: object): boolean {
  *      `null` или `undefined` игнорируются.
  * @returns {Object}
  */
-const extend: (deep?: boolean, target: object, ...objects: object[]) => object =
-    function extend(deep: boolean = false, target: object, ...objects: object[]): object {
-    console.log(target, deep, objects)
+const extend: <T extends boolean | ISomeObject>
+(target: T, firstArg: T extends boolean? ISomeObject: ISomeObject | undefined | null, ...args: (ISomeObject | null | undefined)[])
+    => ISomeObject =
+    function (...args: (boolean | ISomeObject | null | undefined)[]): ISomeObject {
+    let target = args[0] as boolean | ISomeObject;
+    let deep: boolean;
+    let i: number;
 
     // Обрабатываем ситуацию глубокого копирования.
+    if (typeof target === 'boolean') {
+        deep = target as boolean;
+        target = args[1] as ISomeObject;
+        i = 2;
+    } else {
+        deep = false;
+        i = 1;
+    }
 
-    for (let obj of objects) {
+    for (; i < arguments.length; i++) {
+        const obj = args[i] as ISomeObject | null | undefined;
         if (!obj) {
             continue;
         }
 
         for (const key in obj) {
             if (hasOwnProperty.call(obj, key)) {
-                const val: any = obj[key];
+                const val = obj[key];
                 const isArray: boolean = val && Array.isArray(val);
 
                 // Копируем "плоские" объекты и массивы рекурсивно.
                 if (deep && val && (isPlainObject(val) || isArray)) {
                     const src: any = target[key];
-                    let clone: object | any[];
+                    let clone: ISomeObject;
                     if (isArray) {
                         clone = src && Array.isArray(src) ? src : [];
                     } else {
                         clone = src && isPlainObject(src) ? src : {};
                     }
-                    target[key] = extend(deep, clone, val);
+                    target[key] = extend(deep, clone, val as ISomeObject);
                 } else {
                     target[key] = val;
                 }
@@ -60,7 +77,18 @@ const extend: (deep?: boolean, target: object, ...objects: object[]) => object =
         }
     }
 
-    return target;
+    return target as ISomeObject;
 };
+console.log(extend([1, 2, 3], null, [15, 20, { a: 5 }, 147], { 4: 879 }));
 
-export default extend;
+
+extend(true, { a: 5 }, { b: 6 }, { a: 'a', b: 'abc' });
+extend(true, { a: 5 }, { b: 6, c: 123, d:466 });
+extend(false, { a: 5 }, { b: 6 }, false);
+extend(true, { a: 5 }, null, { b: 6 }, null, undefined, {c: 123, d: 435});
+extend({ a: 5 }, { b: 6 }, 123, );
+extend({ a: 5 }, null, { a: 10, b: 20 }, { a: [1,2,3] }, [4,5,6] );
+extend({ a: 5, b: 123 }, { b: 6 }, );
+extend(true, null, { a: 5 }, { b: 6 });
+extend(['asd', 123, ','], null, { a: 5 }, { b: 6 });
+extend(undefined, null, { a: 5 }, { b: 6 });
